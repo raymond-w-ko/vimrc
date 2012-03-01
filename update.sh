fb957988fbf7fcffbb9ca1230d40d4c746576d3c
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-02-29 22:36:07 -0500

    added a delay so it doesn't cause dest servers to think it's a DOS attack

--- update.sh
+++ update.sh
@@ -1,2 +1,19 @@
 #! /bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+cd vimfiles/bundle
+
+for D in `ls | grep -v set_upstreams.sh`
+do
+    echo
+    echo updating $D
+
+    cd $D
+    git pull
+    sleep 5
+    cd ..
+done
+
+exit
+
 git submodule foreach git pull
